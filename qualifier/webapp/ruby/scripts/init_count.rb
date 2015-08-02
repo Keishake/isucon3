e338bfc252cdbf6ed26b38da5cafe35f59e8c403
Keishake <keishake0925@gmail.com>
2015-08-02 17:22:14 +0900

    fix script bug

--- qualifier/webapp/ruby/scripts/init_count.rb
+++ qualifier/webapp/ruby/scripts/init_count.rb
@@ -1,6 +1,16 @@
 require 'dalli'
+require 'json'
 require 'mysql2-cs-bind'
 
 dc = Dalli::Client.new('localhost:11212')
+config = JSON.parse(IO.read(File.dirname(__FILE__) + "/../../config/#{ ENV['ISUCON_ENV'] || 'local' }.json"))['database']
+mysql = Mysql2::Client.new(
+        :host => config['host'],
+        :port => config['port'],
+        :username => config['username'],
+        :password => config['password'],
+        :database => config['dbname'],
+        :reconnect => true,
+      )
 total = mysql.query("SELECT count(id) AS c FROM memos WHERE is_private=0").first["c"]
 dc.set('total', total)
